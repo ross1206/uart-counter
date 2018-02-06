a19f0b1d7e5d5638397c318211828fe520593235
root <root@DESKTOP-P78ERCQ.localdomain>
2018-02-06 22:12:51 +0800

    modified some formate and gitignore

---
+++ select16.v
@@ -0,0 +1,33 @@
+//	setup file time 2018/2/6
+//	author <ross1206vii@gmail.com>
+
+
+
+module select_16
+(
+	in,
+	sel,
+	out
+);
+
+
+input[15:0] in;
+input[3:0] sel;
+output out;
+
+
+
+always @(*)
+begin
+//	case(sel)
+//		4'b0000:
+//			out = in[0];
+	
+	
+//	endcase
+end
+
+
+
+endmodule
+
--- top.v
+++ top.v
@@ -4,12 +4,12 @@
 
 module top
 (
-	in1,
-	out1
+	input in1,
+	output out1
 );
 
-input in1;
-output out1;
+
+
 
 timer025 t25(.clk_in(in1), .out_025s(out1));
 
