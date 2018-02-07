423c8c6b7f379e3ba975736940f6e017f5859633
Ross1206 <ross1206vii@gmail.com>
2018-02-07 13:55:14 +0800

    make seleter16 selected by timer 0.25s signal

--- select16.v
+++ select16.v
@@ -5,12 +5,27 @@
 
 module select_16
 (
-	input [15:0] in,
-	input [3:0] addr,
+	input [15:0] in,		//输入16位信号 每隔0.25秒就自动选择下一个信号输入
+	input time_025,		//这是个脉冲信号 检测上升沿来确定到了0.25秒 然后地址选择自动跳到下一个选择信号
 	output reg out			//第一次写这个东西原来always块中要求被赋值的必须是reg类型 但是如果编写的确实是逻辑而不是时序的话不会被综合成触发器或锁存器
 );
 
 
+
+reg [3:0] addr;
+
+
+
+always @(posedge time_025)
+begin
+	if(addr == 2'd15)	begin
+		addr <= 2'd0;
+	end
+	else begin
+		addr <= addr + 1;
+	end
+
+end
 
 
 
@@ -50,7 +65,7 @@ begin
 		2'd15:
 			out = in[15];
 		default: 
-			out = 'bx;
+			out = 1'bx;
 	endcase
 end
 
