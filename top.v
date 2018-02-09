3cf7d62f549408780bdfae94f1ce6a6f334158d4
Ross1206 <ross1206vii@gmail.com>
2018-02-09 13:14:33 +0800

    update top-level

--- top.v
+++ top.v
@@ -1,17 +1,23 @@
-//`include "timer025.v"
+//top level file in this project
 
 
 
 module top
 (
-	input in1,
+	input clk_in,
+	input [15:0] sig_in,
 	output out1
 );
 
+wire fre9600_wire;
+wire time025_wire;
+wire sig_selected_wire;
 
 
 
-timer025 t25(.clk_in(in1), .out_025s(out1));
+freq9600	f9600(.clk_in(clk_in), .clk_9600(fre9600_wire));
+timer025 	t25(.clk_in(fre9600_wire), .out_025s(time025_wire));
+select_16 	s16(.in(sig_in), .time_025(time025_wire), .out(sig_selected_wire));
 
 endmodule
 
